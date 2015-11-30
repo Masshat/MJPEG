491c228086392bc0741848abf8546ac262afa259
Massine BITAM <massine.bitam@gmail.com>
2015-11-30 16:32:01 +0100

    fix repo

--- tp1/mjpeg/src/iqzz/iqzz.c
+++ tp1/mjpeg/src/iqzz/iqzz.c
@@ -1,6 +1,7 @@
 #include "../common/jpeg.h"
+#include "iqzz_proto.h"
 
-static int ZZ[] =
+const static uint8_t ZZ[BLOCK_SIZE] =
   { 0,  1,  8, 16,  9,  2,  3, 10, 
     17, 24, 32, 25, 18, 11,  4,  5, 
     12, 19, 26, 33, 40, 48, 41, 34, 
@@ -13,8 +14,22 @@ static int ZZ[] =
 
 FUNC(iqzz)
 {
-
+	uint8_t i, j;
     srl_mwmr_t input_mwmr = GET_ARG(input);
-    srl_mwmr_t output = GET_ARG(output);
+    srl_mwmr_t output_mwmr = GET_ARG(output);
     srl_mwmr_t huffman_mwmr = GET_ARG(quanti);
+
+	uint8_t quantif [BLOCK_SIZE];
+	int16_t Fn [BLOCK_WIDTH * BLOCK_HEIGHT];
+	int32_t Fns [BLOCK_WIDTH * BLOCK_HEIGHT]; // Fns represente  Fn'.
+
+	srl_mwmr_read(huffman_mwmr,(void *)quantif , BLOCK_SIZE *sizeof(uint8_t));
+
+	for (i=0; i<NBLOCKS; i++){
+		
+		srl_mwmr_read(input_mwmr,(void *)Fn , BLOCK_SIZE *sizeof(uint16_t));
+		for (j=0; j<BLOCK_SIZE; j++)
+			Fns[ZZ[j]] = Fn[j]*quantif[j];
+		srl_mwmr_write(output_mwmr,(void *)Fns , BLOCK_SIZE *sizeof(uint32_t));
+	}
 }
