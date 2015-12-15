44c46fa892a1d3481084bed062b397cfd4d307c1
Massine BITAM <massine.bitam@gmail.com>
2015-12-15 13:41:22 +0100

    TP3 is done :(

---
+++ tp4/mjpeg/src/split/split.c
@@ -0,0 +1,15 @@
+#include "srl.h"
+#include "demux_proto.h"
+#include "srl_endianness.h"
+
+#include "../common/jpeg.h"
+
+#include "../common/block_io.h"
+
+FUNC(split) {
+
+    srl_mwmr_t input_mwmr = GET_ARG(input);
+    srl_mwmr_t output_mwmr = GET_ARG(output1);
+    srl_mwmr_t output_mwmr = GET_ARG(output2);
+
+}
