e102f21b5832cf846fc67bd5cef385636461bfce
Massine BITAM <massine.bitam@gmail.com>
2015-12-31 13:10:51 +0100

    the fucking posix implementation is working

--- tp4/mjpeg/src/libu/libu.c
+++ tp4/mjpeg/src/libu/libu.c
@@ -1,33 +1,26 @@
-#include <srl.h>
+#include "../common/jpeg.h"
 #include "libu_proto.h"
 
-#include "../common/jpeg.h"
-
+#define size BLOCK_WIDTH * WIDTH
 FUNC(libu)
 {
-    srl_mwmr_t input[2] = { GET_ARG(input0), GET_ARG(input1) };
-    srl_mwmr_t output = GET_ARG(output);
+	uint8_t i, j, z;
+    srl_mwmr_t input_mwmr[2] = {GET_ARG(input1),GET_ARG(input2)};
+    srl_mwmr_t output_mwmr = GET_ARG(output);
 
-    uint8_t in[BLOCK_SIZE];
-    uint8_t out[WIDTH * BLOCK_HEIGHT];
-    int i, j, k, l;
-    int which = 0;
-
-    srl_log(TRACE, "LIBU thread is alive!\n");
-
-    while (1) {
-        for ( i = 0; i < BLOCKS_H; i++ ) {
-            for ( j = 0; j < BLOCKS_W; j++ ) {
-                srl_mwmr_read( input[which], in, sizeof(in) );
-
-                for ( k = 0; k < BLOCK_HEIGHT; k++ )
-                    for ( l = 0; l < BLOCK_WIDTH; l++ )
-                        out[k * WIDTH + j * BLOCK_WIDTH + l] = in[k * BLOCK_WIDTH + l];
-            }
-
-            srl_mwmr_write( output, out, sizeof(out));
-        }
-
-        which = !which;
-    }
+	int8_t bloc [BLOCK_WIDTH * BLOCK_HEIGHT];
+	int8_t buffer [size];
+	int8_t pipe = 0;
+	while (1){
+	for (i=0; i<BLOCKS_H; i++){
+		for (j=0; j<BLOCKS_W; j++){
+			srl_mwmr_read(input_mwmr[pipe],(void *)bloc , BLOCK_SIZE);
+			for (z=0; z<BLOCK_WIDTH; z++)
+				memcpy(&buffer[j*BLOCK_WIDTH+z*WIDTH],&bloc[BLOCK_WIDTH*z],BLOCK_WIDTH);
+		}
+			
+		srl_mwmr_write(output_mwmr, (void *)buffer, size *sizeof(uint8_t));
+	}
+	pipe = ! pipe;
+}
 }
--- tp4/mjpeg/src/split/split.c
+++ tp4/mjpeg/src/split/split.c
@@ -1,51 +1,39 @@
-#include <srl.h>
+#include "srl.h"
 #include "split_proto.h"
+#include "srl_endianness.h"
 
-#include "../common/block_io.h"
 #include "../common/jpeg.h"
+#include "../common/block_io.h"
 
-FUNC(split)
-{
-    srl_mwmr_t input = GET_ARG(input);
-    srl_mwmr_t output[2] = { GET_ARG(output0), GET_ARG(output1) };
+FUNC(split) {
 
-    int which = -1;
-    uint8_t in[BLOCK_SIZE];
-    uint8_t out[BLOCK_SIZE];
-    block_io bin;
-    block_io bout[2];
+	srl_mwmr_t input_mwmr   = GET_ARG(input);
+    	srl_mwmr_t output_mwmr[2] = {GET_ARG(output1), GET_ARG(output2)};
 
-    srl_log(TRACE, "SPLIT thread is alive!\n");
+	uint8_t input_buffer[32];
+	uint8_t output_buffer[32];
 
-    block_io_init_in(&bin, in, sizeof(in), input);
-    block_io_init_out(&bout[0], out, sizeof(out), output[0]);
-    block_io_init_out(&bout[1], out, sizeof(out), output[1]);
+	block_io in, out[2];
 
-    while (1) {
-        uint8_t b = block_io_read_int8(&bin);
-
-        if (b != 0xFF) {
-            block_io_write_int8(&bout[which], b);
-        } else {
-            uint8_t m = block_io_read_int8(&bin);
-
-            if (m == 0xD8) {
-                int i;
-
-                if (which >= 0) {
-                    int left = bout[which].left;
-                    
-                    for (i = 0; i < left; i++)
-                        block_io_write_int8(&bout[which], 0xFF);
-
-                    block_io_flush(&bout[which]);
-                }
-                
-                which = !which;
-            }
-
-            block_io_write_int8(&bout[which], b);
-            block_io_write_int8(&bout[which], m);
-        }
-    }
+	block_io_init_in  (&in    , input_buffer , 32, input_mwmr);
+	block_io_init_out (&out[0], output_buffer, 32, output_mwmr[0]);
+	block_io_init_out (&out[1], output_buffer, 32, output_mwmr[1]);
+	
+	uint8_t b, m;
+	uint8_t  pipe = 1;
+	while(1){
+		b = block_io_read_int8(&in);
+		if (b == 0xFF){	
+			m = block_io_read_int8(&in);
+			if (m == 0xD8){
+				block_io_set_left(&out[pipe],0xFF);
+				block_io_flush(&out[pipe]);
+				pipe = (pipe +1) % 2;
+			}
+			block_io_write_int8(&out[pipe], b);
+			block_io_write_int8(&out[pipe], m);
+			continue;
+		}
+		block_io_write_int8(&out[pipe], b);
+	}
 }
